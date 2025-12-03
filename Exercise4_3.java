b07d14a665a6b68c7d9ffc27863996ec8d146f85
sulenurbaygut <250541029@firat.edu.tr>
2025-12-03 20:43:38 +0300

    Update Exercise4_3.java eklendi

--- Exercise4_3.java
+++ Exercise4_3.java
@@ -1,24 +1,28 @@
+öğrenci no : 250541029
+    ad-soyad: şule nur bayğut
+    /**
+ * Exercise4_3.java
+ * Stack diyagrami ve cikti analizi.
+ */
 public class Exercise4_3 {
+    public static void ping(String bob) {
+        System.out.println(bob);
+        baffle("pong");
+    }
 
-    public static void zoop() {
-        baffle();
-        System.out.print("You wugga ");
-        baffle();
+    public static void baffle(String blimp) {
+        System.out.println(blimp);
+        zippo("ping", -5);
+    }
+
+    public static void zippo(String quux, int flag) {
+        if (flag < 0) {
+            System.out.println(quux + " zoop");
+        }
     }
 
     public static void main(String[] args) {
-        System.out.print("No, I ");
-        zoop();
-        System.out.print("I ");
-        baffle();
-    }
-
-    public static void baffle() {
-        System.out.print("wug");
-        ping();
-    }
-
-    public static void ping() {
-        System.out.println(".");
+        String alice = "hello";
+        ping(alice);
     }
 }
