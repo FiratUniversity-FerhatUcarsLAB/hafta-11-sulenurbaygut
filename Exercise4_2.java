24c693300f2bb39147d142048b84af95b5a739ff
sulenurbaygut <250541029@firat.edu.tr>
2025-12-03 20:42:47 +0300

    Update Exercise4_2.java eklendi

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -1,23 +1,27 @@
+öğrenci no :250541029
+  ad-soyad: şule nur bayğut 
+  /**
+ * Exercise4_2.java
+ * Calisma sirasini gosterir.
+ */
 public class Exercise4_2 {
-
-  // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
-
-    public static void main(String[] args) {
-        zippo("rattle", 13);
+    public static void baffle(String blimp) { // 3, 9, 13
+        System.out.println(blimp);           // 4, 10, 14
+        zippo("ping", -5);                   // 5, 11
     }
 
-    public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
-    }
-
-    public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
+    public static void zippo(String quux, int flag) { // 6, 12
+        if (flag < 0) {                      // 7, 13
+            System.out.println(quux + " zoop"); // 8
         } else {
             System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
+            baffle(quux);
+            System.out.println("boo-wa-ha");
         }
+    }
+
+    public static void main(String[] args) { // 1
+        baffle("woof");                      // 2
+        System.out.println("rick");          // 16
     }
 }
